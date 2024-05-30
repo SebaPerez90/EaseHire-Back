e16c6c131f5b3be3be6a372eac24338ca0a0a043
Sebastian Perez <112594443+SebaPerez90@users.noreply.github.com>
2024-05-30 10:10:57 -0300

    create-folder|auth

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,10 +3,11 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { Credential } from 'src/database/entities/credentials.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthRepository } from './auth.repository';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Credential])],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, AuthRepository],
 })
 export class AuthModule {}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { AuthRepository } from './auth.repository';
+import { RegisterDto } from './dto/register.dto';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private credentialsRepository: AuthRepository) {}
+
+  async simulateSignup(credentials: RegisterDto) {
+    return await this.credentialsRepository.simulateAuthFlow(credentials);
+  }
+}
