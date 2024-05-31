93329a416ca14d866567b6d86060135d451d0afb
Sebastian Perez <112594443+SebaPerez90@users.noreply.github.com>
2024-05-31 17:15:20 -0300

    mockdata-education

--- src/modules/education/education.module.ts
+++ src/modules/education/education.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { EducationController } from './education.controller';
 import { EducationService } from './education.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Education } from 'src/database/entities/education.entity';
+import { UserRepository } from '../users/users.repository';
+import { User } from 'src/database/entities/user.entity';
+import { AuthRepository } from '../auth/auth.repository';
+import { Credential } from 'src/database/entities/credentials.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Education, User, Credential])],
   controllers: [EducationController],
-  providers: [EducationService],
+  providers: [EducationService, UserRepository, AuthRepository],
 })
 export class EducationModule {}
