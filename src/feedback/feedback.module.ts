89fc4cee3c282f3329b30e7ca9c01257256efeb9
Yarod Bonilla <yarold_br@outlook.es>
2024-05-30 22:25:49 -0500

    falta

--- src/feedback/feedback.module.ts
+++ src/feedback/feedback.module.ts
@@ -6,9 +6,10 @@ import { Feedback } from 'src/database/entities/feedback.entity';
 import { FeedbackRepository } from './feedback.repository';
 import { Profesion } from 'src/database/entities/profesion.entity';
 import { ProfesionsRepository } from 'src/profesions/profesions.repository';
+import { User } from 'src/database/entities/user.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Feedback, Profesion])],
+  imports: [TypeOrmModule.forFeature([Feedback, Profesion,User])],
   controllers: [FeedbackController],
   providers: [FeedbackService, FeedbackRepository],
 })
--- src/feedback/feedback.repository.ts
+++ src/feedback/feedback.repository.ts
@@ -3,29 +3,40 @@ import { Feedback } from 'src/database/entities/feedback.entity';
 import { Repository } from 'typeorm';
 import { CreateFeedbackDto } from './dto/create-feedback.dto';
 import { Profesion } from 'src/database/entities/profesion.entity';
+import { User } from 'src/database/entities/user.entity';
 
 export class FeedbackRepository {
   constructor(
     @InjectRepository(Feedback)
     private feedbackRepository: Repository<Feedback>,
     @InjectRepository(Profesion)
-    private profesionRepository: Repository<Profesion>
+    private profesionRepository: Repository<Profesion>,
+    @InjectRepository(User)
+    private userRepository: Repository<User>,
   ) {}
   async create(feedback: CreateFeedbackDto) {
-    const {rate,description,profesionId} = feedback
-    const profesion = await this.profesionRepository.findOne({ where: { id: profesionId } })
+    const { rate, description, profesionId } = feedback;
+    const profesion = await this.profesionRepository.findOne({
+      where: { id: profesionId },
+    });
 
-    const feedbackcreate = new Feedback()
+    const feedbackcreate = new Feedback();
     feedbackcreate.rate = rate;
     feedbackcreate.description = description;
-    feedbackcreate.profesion = profesion
+    feedbackcreate.profesion = profesion;
     await this.feedbackRepository.save(feedbackcreate);
+    const findprofesion = await this.profesionRepository.findOneBy(
+      feedbackcreate.profesion,
+    );
+    console.log(findprofesion);
+
+    console.log(findprofesion.user);
 
     return feedbackcreate;
   }
   async getAll() {
     const feedback = await this.feedbackRepository.find({
-      relations: ['profesion'],
+      relations: ['profesion', 'profesion.user'],
     });
     return feedback;
   }
