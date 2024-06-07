e3f984849e206556ef70173a1177e3cdb0995708
Sebastian Perez <112594443+SebaPerez90@users.noreply.github.com>
2024-06-07 18:34:18 -0300

    .

--- src/main.ts
+++ src/main.ts
@@ -22,7 +22,7 @@ async function bootstrap() {
     .addBearerAuth()
     .build();
   const document = SwaggerModule.createDocument(app, swaggerConfig);
-  app.useGlobalPipes(new ValidationPipe())
+  app.useGlobalPipes(new ValidationPipe());
   SwaggerModule.setup('api', app, document);
   await app.listen(port);
 }
