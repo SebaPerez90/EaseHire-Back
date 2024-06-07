2cb46f56575d07e16618ddfbcc89cd81c780e50d
Yarod Bonilla <yarold_br@outlook.es>
2024-06-07 16:33:07 -0500

    tokenfuncion

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -10,7 +10,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Credential } from 'src/database/entities/credentials.entity';
 import { JwtService } from '@nestjs/jwt';
 import { User } from 'src/database/entities/user.entity';
-import { v4 as uuidv4 } from 'uuid'
+import { v4 as uuidv4 } from 'uuid';
 @Injectable()
 export class AuthService {
   constructor(
@@ -18,49 +18,41 @@ export class AuthService {
     private credentialRepository: Repository<Credential>,
     private readonly jwtService: JwtService,
     @InjectRepository(User)
-    private userRepository: Repository<User>
+    private userRepository: Repository<User>,
   ) {}
 
   async signIn(credentials) {
     try {
-      
-      
-      const {  email, given_name,name
-        , family_name, picture,email_verified } = credentials;
-      
-        let user = await this.userRepository.findOne({ where: { email: email } })
-        if (!user) {
-          user = await this.userRepository.create({
-            id: uuidv4(),
-            name: name?name:null,
-            lastName: family_name ? family_name : null,
-            email: email,
-            email_verified: email_verified? email_verified:null,
-            imgPictureUrl: picture ? picture : null
-            
-            
-          })
-          this.userRepository.save(user)
-          
-          
-        }
-        const userid = user.id
+      const { email, given_name, name, family_name, picture, email_verified } =
+        credentials;
 
-        const playload = {userid,email };
-        const token = this.jwtService.sign(playload, { secret: process.env.JWT_SECRET });
-       console.log(token);
-       
+      let user = await this.userRepository.findOne({ where: { email: email } });
+      if (!user) {
+        user = await this.userRepository.create({
+          id: uuidv4(),
+          name: name ? name : null,
+          lastName: family_name ? family_name : null,
+          email: email,
+          email_verified: email_verified ? email_verified : null,
+          imgPictureUrl: picture ? picture : null,
+        });
+        this.userRepository.save(user);
+      }
+      const userid = user.id;
+
+      const playload = { userid, email };
+      const token = this.jwtService.sign(playload, {
+        secret: process.env.JWT_SECRET,
+      });
+
       return {
         message: 'User login',
-        token
-    }
-      
+        token,
+      };
     } catch (error) {
-        
-    console.error('Error en signIn:', error);
-    throw new BadRequestException('failed to login'); 
-  }
-    
+      console.error('Error en signIn:', error);
+      throw new BadRequestException('failed to login');
+    }
   }
   signUp(credential: RegisterDto) {
     try {
