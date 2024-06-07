9693cf0ada0a1871090d3bb8930363071d1716b9
Sebastian Perez <112594443+SebaPerez90@users.noreply.github.com>
2024-06-07 17:54:31 -0300

    update

--- src/main.ts
+++ src/main.ts
@@ -9,8 +9,7 @@ const port = process.env.PORT || 3001;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    //modificar esto para acepte el puerto 3000 asi labura el front
-    origin: 'http://localhost:5173', // Origen del front
+    origin: 'http://localhost:3000', // Origen del front
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
