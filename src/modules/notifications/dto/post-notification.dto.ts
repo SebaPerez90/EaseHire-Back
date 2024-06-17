99ebd85bc5bc8f0cabd0269467ef7fb27de3800f
Sebastian Perez <112594443+SebaPerez90@users.noreply.github.com>
2024-06-16 23:05:54 -0300

    notification-flow

--- src/modules/notifications/dto/post-notification.dto.ts
+++
@@ -1,7 +0,0 @@
-// import { IsEnum } from 'class-validator';
-// import { NotificationType } from 'src/enum/notification.enum';
-
-// export class PostNotificationDto {
-//   @IsEnum(NotificationType)
-//   type: NotificationType;
-// }
