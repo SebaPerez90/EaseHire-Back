6c09c3108c929ed13973da399754873dc9d08412
Sebastian Perez <112594443+SebaPerez90@users.noreply.github.com>
2024-06-18 18:55:39 -0300

    .

--- src/modules/profesions/profesions.controller.ts
+++ src/modules/profesions/profesions.controller.ts
@@ -11,6 +11,7 @@ import {
   ParseIntPipe,
   Headers,
   NotFoundException,
+  Req,
 } from '@nestjs/common';
 import { ProfesionsService } from './profesions.service';
 import { CreateProfesionDto } from './dto/create-profesion.dto';
@@ -20,6 +21,7 @@ import { JwtService } from '@nestjs/jwt';
 import { Roles } from 'src/decorators/role.decorator';
 import { Role } from 'src/enum/role.enum';
 import { Public } from 'src/decorators/is-public.decorator';
+import { Request } from 'express';
 
 @ApiTags('profesions')
 @Controller('profesions')
@@ -67,6 +69,12 @@ export class ProfesionsController {
     return this.profesionsService.update(id, updateProfesionDto);
   }
 
+  @Delete('update/:idd')
+  removeProfesion(@Req() req: Request, @Param('idd') profesionID: string) {
+    // removeProfesion() {
+    return this.profesionsService.removeProfesion(req, profesionID);
+    // return 'pepe';
+  }
   @Delete(':id')
   @Roles(Role.ADMIN)
   async remove(@Param('id') id: string) {
